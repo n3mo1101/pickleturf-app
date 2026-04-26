aafae81244d6649a2279f01a38d9e2182bc022f4
n3mo1101 <rd.cruz1101@gmail.com>
2026-04-26 23:54:02 +0800

    Update frontend
    - Updated navbar
    - Added temporary app urls and page templates

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'bookings'
 
 urlpatterns = [
-    
+    path('', TemplateView.as_view(template_name='coming_soon.html'), name='list'),
 ]
